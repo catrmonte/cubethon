783a8bbf3e23ba2b654460271e7057af4a6a6df3
catrmonte <59862274+catrmonte@users.noreply.github.com>
2020-01-15 08:49:34 -0700

    Add files via upload

---
+++ WebBuild1/Scripts/PlayerCollision.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public PlayerMovement movement;
+
+    void OnCollisionEnter(Collision collisionInfo) // gets info on item colliding into
+    {
+        if (collisionInfo.collider.tag == "Obstacle") // check to see if it's Obstacle
+        {
+            GetComponent<PlayerMovement>().enabled = false;
+
+            FindObjectOfType<GameManager>().EndGame();
+        }
+    }
+
+}
